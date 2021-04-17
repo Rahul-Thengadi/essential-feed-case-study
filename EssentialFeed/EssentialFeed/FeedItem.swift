7c17de0f00e2b43ca36ba7316f37585b717e3137
Rahul <thengadir@gmail.com>
2021-04-17 11:51:02 +0530

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Rahul Thengadi on 17/04/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
